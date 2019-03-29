f7e0e7d8785643f6377205f0e9ce38fccb69f2b6
Adrian Cruz <drincruz@gmail.com>
2019-03-29 00:02:14 -0400

    Working `node` Erlang module
    Quick first pass of an Erlang module to hold arbitrary data and children.
    Small .editorconfig update following style guide found [here](https://github.com/inaka/erlang_guidelines).

--- src/node.erl
+++ src/node.erl
@@ -1,14 +1,33 @@
 %% A node is one piece of a graph and can have many other nodes.
 -module(node).
--export([init/1, init/2]).
-% -compile(export_all).
+-export([new/1, new/2, append_children/2]).
 
 -record(node, {
-    value=nil,
-    children=[]
+    value = nil,
+    children = []
 }).
 
-init(Value) ->
-    init(Value, []).
-init(Value, Children) ->
+new(Value) ->
+    new(Value, []).
+new(Value, Children) when is_list(Children) ->
     #node{value = Value, children = Children}.
+
+append_children(Node, Child) when is_record(Child, node) ->
+    append_children(Node, [Child]);
+append_children(Node, [Child] = Children) ->
+    case lists:member(Child, Node#node.children) of
+        true ->
+            Node;
+        false ->
+            Node#node{children = lists:append(Children, Node#node.children)}
+    end;
+append_children(Node, Children) when is_list(Children) ->
+    Node#node{children = [child_appender(Node#node.children, C) || C <- Children ]}.
+
+child_appender(Children, Child) when is_list(Children), is_record(Child, node) ->
+    case lists:member(Child, Children) of
+        true ->
+            Children;
+        false ->
+            lists:append(Children, Child)
+    end.
